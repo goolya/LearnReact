bf9e356303cbd349dc160e99054a22614bda0fff
goolya <goolya@users.noreply.github.com>
2017-10-03 14:23:59 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-  import React, { Component } from 'react';
+import React, { Component } from 'react';
 import Projects from './components/projects';
 import AddProject from './components/AddProject';
 import Todos from './components/Todos';
